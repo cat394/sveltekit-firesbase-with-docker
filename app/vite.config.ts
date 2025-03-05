503492de29406b66900b4fd13d6f71630f047d82
cat394 <keyring1231@gmail.com>
2025-03-06 08:48:46 +0900

    setup docker for sveltekit app

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,7 +4,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
+	server: {
+		watch: {
+			usePolling: true
+		}
+	},
 	test: {
 		workspace: [
 			{
